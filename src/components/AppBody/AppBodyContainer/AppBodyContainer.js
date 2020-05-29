f5bd6f34786a3c2e5ddfcdc27c6efd1cf3944cce
Adam Martin <>
2020-05-29 10:58:35 -0700

    Another test live commit.

--- src/components/AppBody/AppBodyContainer/AppBodyContainer.js
+++ src/components/AppBody/AppBodyContainer/AppBodyContainer.js
@@ -3,7 +3,7 @@ import PagePrimaryHeading from '../PagePrimaryHeading/PagePrimaryHeading';
 import AppLogicContainer from './AppLogicContainer/AppLogicContainer';
 import classes from '../AppBodyContainer/AppBodyContainer.css';
 
-const appTitle = 'React Unit Converter';
+const appTitle = 'Unit Converter';
 
 
 const appBodyContainer = (props) => (
